f1f33cf8e78215c4c92b5af7fc909a8a2e4d3026
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-04 01:22:14 +0800

    Add TGW subnet and route table resources

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -19,3 +19,28 @@ resource "aws_subnet" "private" {
     Name = "${var.name_prefix}-private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_subnet" "tgw" {
+  count             = length(var.availability_zones)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index + 8)
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.name_prefix}-tgw-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name_prefix}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
