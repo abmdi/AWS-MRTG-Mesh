a9e49e55df4906564352eb15fe6f040b82c79838
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-04 01:19:34 +0800

    Add variables for name prefix and availability zones

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -7,3 +7,13 @@ variable "environment" {
   description = "Environment tag"
   type        = string
 }
+
+variable "name_prefix" {
+  description = "Prefix for resource naming"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of Availability Zones"
+  type        = list(string)
+}
