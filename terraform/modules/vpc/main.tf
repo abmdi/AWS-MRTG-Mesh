c31f6035e8d70e5f87d2e65fd691d9128b8dc35f
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-04 01:20:22 +0800

    Add VPC and private subnet resources in Terraform

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,1 +1,21 @@
 
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.availability_zones)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, count.index)
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.name_prefix}-private-subnet-${count.index + 1}"
+  }
+}
