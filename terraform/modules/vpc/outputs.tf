020a9db137cf41bb92db6c457c0a63cede732714
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-04 01:23:17 +0800

    Add outputs for VPC and related resources

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,1 +1,16 @@
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "tgw_subnet_ids" {
+  value = aws_subnet.tgw[*].id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
