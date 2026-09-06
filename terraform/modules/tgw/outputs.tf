e2e34a8bed0127e11c8cdccf41325c845f600bb3
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-07 00:07:53 +0800

    Add outputs for Transit Gateway configuration
    Added outputs for Transit Gateway details including ID, ARN, VPC attachment ID, and route table IDs.

--- terraform/modules/tgw/outputs.tf
+++ terraform/modules/tgw/outputs.tf
@@ -1,1 +1,25 @@
 
+output "tgw_id" {
+  description = "The ID of the Transit Gateway"
+  value       = aws_ec2_transit_gateway.main.id
+}
+
+output "tgw_arn" {
+  description = "The ARN of the Transit Gateway"
+  value       = aws_ec2_transit_gateway.main.arn
+}
+
+output "vpc_attachment_id" {
+  description = "The ID of the TGW VPC Attachment"
+  value       = aws_ec2_transit_gateway_vpc_attachment.main.id
+}
+
+output "workload_route_table_id" {
+  description = "The ID of the Workload TGW Route Table"
+  value       = aws_ec2_transit_gateway_route_table.workload_rt.id
+}
+
+output "peering_route_table_id" {
+  description = "The ID of the Peering TGW Route Table"
+  value       = aws_ec2_transit_gateway_route_table.peering_rt.id
+}
