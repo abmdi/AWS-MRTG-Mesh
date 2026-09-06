0dd4229d005fa2293b911f32eabac6a764296119
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-06 22:57:16 +0800

    Add variables for TGW resource configuration

--- terraform/modules/tgw/variables.tf
+++ terraform/modules/tgw/variables.tf
@@ -1,1 +1,10 @@
+variable "name_prefix" {
+  description = "Prefix for TGW resource naming"
+  type        = string
+}
 
+variable "amazon_side_asn" {
+  description = "BGP Autonomous System Number (ASN) for the Transit Gateway"
+  type        = number
+  default     = 64512
+}
