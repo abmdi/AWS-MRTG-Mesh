e7a7926594fef619ec23fb96d7b857be3bc6958d
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-06 23:00:20 +0800

    Add new variables for TGW configuration

--- terraform/modules/tgw/variables.tf
+++ terraform/modules/tgw/variables.tf
@@ -8,3 +8,18 @@ variable "amazon_side_asn" {
   type        = number
   default     = 64512
 }
+
+variable "vpc_id" {
+  description = "Target VPC ID for TGW Attachment"
+  type        = string
+}
+
+variable "tgw_subnet_ids" {
+  description = "List of TGW dedicated subnet IDs within the VPC"
+  type        = list(string)
+}
+
+variable "remote_vpc_cidr" {
+  description = "Destination VPC CIDR block for static peering routing"
+  type        = string
+}
