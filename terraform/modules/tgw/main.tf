984b22620b01e1f1c7f0eb1dc9ead84b9dbda2a4
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-06 23:02:27 +0800

    Add AWS Transit Gateway and VPC attachment resources
    Added AWS Transit Gateway and VPC attachment resources with necessary configurations and tags.

--- terraform/modules/tgw/main.tf
+++ terraform/modules/tgw/main.tf
@@ -1,1 +1,27 @@
 
+# AWS Transit Gateway Resource
+resource "aws_ec2_transit_gateway" "main" {
+  description                     = "Production TGW for ${var.name_prefix}"
+  amazon_side_asn                 = var.amazon_side_asn
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+  dns_support                     = "enable"
+  vpn_ecmp_support                = "enable"
+
+  tags = {
+    Name = "${var.name_prefix}-tgw"
+  }
+}
+
+# TGW VPC Attachment
+resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+  vpc_id             = var.vpc_id
+  subnet_ids         = var.tgw_subnet_ids
+
+  dns_support = "enable"
+
+  tags = {
+    Name = "${var.name_prefix}-tgw-attachment"
+  }
+}
