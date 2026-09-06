8b9362be49f6f5ecf0032d979099fc7c1c1f74d2
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-06 23:11:14 +0800

    Add TGW route tables and associations
    Added custom Transit Gateway route tables for workloads and inter-region peering, along with associations and propagation resources.

--- terraform/modules/tgw/main.tf
+++ terraform/modules/tgw/main.tf
@@ -25,3 +25,33 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
     Name = "${var.name_prefix}-tgw-attachment"
   }
 }
+
+# Custom TGW Route Table for Workloads
+resource "aws_ec2_transit_gateway_route_table" "workload_rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+
+  tags = {
+    Name = "${var.name_prefix}-workload-tgw-rt"
+  }
+}
+
+# Custom TGW Route Table for Inter-Region Peering
+resource "aws_ec2_transit_gateway_route_table" "peering_rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.main.id
+
+  tags = {
+    Name = "${var.name_prefix}-peering-tgw-rt"
+  }
+}
+
+# TGW Route Table Association
+resource "aws_ec2_transit_gateway_route_table_association" "workload_assoc" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.main.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_rt.id
+}
+
+# TGW Route Table Propagation
+resource "aws_ec2_transit_gateway_route_table_propagation" "workload_prop" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.main.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.workload_rt.id
+}
