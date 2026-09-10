4b619c3623405d579cf26f278338ba294213cf7f
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-10 23:01:58 +0800

    Implement inter-region TGW peering and route updates
    Added resources for inter-region Transit Gateway peering and route table updates between primary and secondary regions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,3 +71,74 @@ module "tgw_secondary" {
   tgw_subnet_ids  = module.vpc_secondary.tgw_subnet_ids
   remote_vpc_cidr = var.primary_vpc_cidr
 }
+# -----------------------------------------------------------------------------
+# 3. INTER-REGION TGW PEERING ATTACHMENT
+# -----------------------------------------------------------------------------
+
+# Initiator: Create Peering Request from Primary Region to Secondary Region
+resource "aws_ec2_transit_gateway_peering_attachment" "primary_to_secondary" {
+  provider = aws.us_east_1
+
+  transit_gateway_id      = module.tgw_primary.tgw_id
+  peer_transit_gateway_id = module.tgw_secondary.tgw_id
+  peer_region             = var.secondary_region
+
+  tags = {
+    Name = "${var.environment}-tgw-peering-us-east-1-eu-west-1"
+  }
+}
+
+# Acceptor: Accept Peering Request in Secondary Region
+resource "aws_ec2_transit_gateway_peering_attachment_accepter" "secondary_accepter" {
+  provider = aws.eu_west_1
+
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.primary_to_secondary.id
+
+  tags = {
+    Name = "${var.environment}-tgw-peering-accepter"
+  }
+}
+
+# -----------------------------------------------------------------------------
+# 4. TGW ROUTE TABLE ENTRIES FOR INTER-REGION PEERING
+# -----------------------------------------------------------------------------
+
+# Route in Primary TGW pointing to Secondary VPC CIDR via Peering Attachment
+resource "aws_ec2_transit_gateway_route" "primary_to_secondary_route" {
+  provider = aws.us_east_1
+
+  destination_cidr_block         = var.secondary_vpc_cidr
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.primary_to_secondary.id
+  transit_gateway_route_table_id = module.tgw_primary.workload_route_table_id
+}
+
+# Route in Secondary TGW pointing to Primary VPC CIDR via Peering Attachment
+resource "aws_ec2_transit_gateway_route" "secondary_to_primary_route" {
+  provider = aws.eu_west_1
+
+  destination_cidr_block         = var.primary_vpc_cidr
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment_accepter.secondary_accepter.id
+  transit_gateway_route_table_id = module.tgw_secondary.workload_route_table_id
+}
+
+# -----------------------------------------------------------------------------
+# 5. VPC LOCAL ROUTE TABLE UPDATES
+# -----------------------------------------------------------------------------
+
+# Add route in Primary VPC Private Route Table towards Primary TGW
+resource "aws_route" "primary_vpc_to_tgw" {
+  provider = aws.us_east_1
+
+  route_table_id         = module.vpc_primary.private_route_table_id
+  destination_cidr_block = var.secondary_vpc_cidr
+  transit_gateway_id     = module.tgw_primary.tgw_id
+}
+
+# Add route in Secondary VPC Private Route Table towards Secondary TGW
+resource "aws_route" "secondary_vpc_to_tgw" {
+  provider = aws.eu_west_1
+
+  route_table_id         = module.vpc_secondary.private_route_table_id
+  destination_cidr_block = var.primary_vpc_cidr
+  transit_gateway_id     = module.tgw_secondary.tgw_id
+}
