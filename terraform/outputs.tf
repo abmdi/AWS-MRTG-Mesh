e19e219faf55d93bbd20c4c7777a0b79e8868cd0
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-10 23:02:50 +0800

    Add outputs for VPC and Transit Gateway IDs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,24 @@
+output "primary_vpc_id" {
+  description = "Primary Region VPC ID"
+  value       = module.vpc_primary.vpc_id
+}
 
+output "secondary_vpc_id" {
+  description = "Secondary Region VPC ID"
+  value       = module.vpc_secondary.vpc_id
+}
+
+output "primary_tgw_id" {
+  description = "Primary Transit Gateway ID"
+  value       = module.tgw_primary.tgw_id
+}
+
+output "secondary_tgw_id" {
+  description = "Secondary Transit Gateway ID"
+  value       = module.tgw_secondary.tgw_id
+}
+
+output "tgw_peering_attachment_id" {
+  description = "Inter-Region TGW Peering Attachment ID"
+  value       = aws_ec2_transit_gateway_peering_attachment.primary_to_secondary.id
+}
