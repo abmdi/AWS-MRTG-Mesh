f5e06173781f0e88b70576158bb85a63e6733c74
Abbas Moradi (Erik) <134613421+abmdi@users.noreply.github.com>
2026-09-10 21:38:26 +0800

    Fetch availability zones for Region A and B
    Added data sources to fetch available availability zones for two regions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+# Fetch Available Availability Zones for Region A
+data "aws_availability_zones" "primary_az" {
+  provider = aws.us_east_1
+  state    = "available"
+}
 
+# Fetch Available Availability Zones for Region B
+data "aws_availability_zones" "secondary_az" {
+  provider = aws.eu_west_1
+  state    = "available"
+}
